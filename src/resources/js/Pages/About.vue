ff3772641721f1d73073ef9ce267e632a5ac0ec8
LittleTruck <i839855314i@gmail.com>
2021-06-30 15:22:41 +0800

    Upload code

---
+++ src/resources/js/Pages/About.vue
@@ -0,0 +1,17 @@
+﻿<template>
+    <div class="about">
+        <h2>
+            About Page
+        </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style scoped>
+
+</style>
