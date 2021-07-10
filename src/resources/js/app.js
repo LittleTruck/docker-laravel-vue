a67772f5e4eb324032b332c69b82bc43b8930aa1
LittleTruck <i839855314i@gmail.com>
2021-07-10 22:20:32 +0800

    install required package

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -7,6 +7,16 @@ import router from "./Router/index";
 import store from "./Store/index";
 import App from './App.vue'
 
+//Import bootstrap
+import 'bootstrap'; // Import js file
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import css file
+import 'bootstrap/dist/css/bootstrap-utilities.min.css'; // Import css file
+
+//Import aos
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
 Vue.use(VueRouter)
 
 const app = new Vue({
